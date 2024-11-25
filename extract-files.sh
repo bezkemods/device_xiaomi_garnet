042004d17a1894abb327e74ac2958cb955148af7
Michael Bestas <mkbestas@lineageos.org>
2024-11-25 15:19:52 -0300

    garnet: Shim libwvhidl with libcrypto_shim
    The following change
    google/boringssl@e202e51
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -96,6 +96,9 @@ function blob_fixup() {
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
             grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;   
     esac
 }
 
