ead2c9c1b89ce078cd71829c5048877301d60b00
Drenzzz <realdrenzzz@gmail.com>
2024-12-24 11:25:25 +0000

    garnet: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,15 @@
 echo 'Cloning stuffs to build for garnet'
 
 # Clone primary tree
-git clone --depth=1 https://github.com/Soverzion-HQ/vendor_xiaomi_garnet -b 15 vendor/xiaomi/garnet
-git clone --depth=1 https://github.com/Soverzion-HQ/device_xiaomi_garnet-kernel -b sk-ksu device/xiaomi/garnet-kernel
+git clone --depth=1 https://github.com/Soverzion-Garnet/vendor_xiaomi_garnet -b 15 vendor/xiaomi/garnet
+git clone --depth=1 https://github.com/Soverzion-Garnet/device_xiaomi_garnet-kernel -b 14 device/xiaomi/garnet-kernel
 
 # Hardware Xiaomi
 rm -rf hardware/xiaomi
-git clone https://github.com/Soverzion-HQ/hardware_xiaomi -b lineage-22 hardware/xiaomi
+git clone https://github.com/Soverzion-Garnet/hardware_xiaomi -b lineage-22 hardware/xiaomi
 git clone https://gitlab.com/Soverzion-HQ/vendor_xiaomi_garnet-miuicamera vendor/xiaomi/garnet-miuicamera
 rm -rf hardware/qcom-caf/sm8450/display
-git clone https://github.com/Soverzion-HQ/hardware_qcom-caf_sm8450_display -b 14 hardware/qcom-caf/sm8450/display
-git clone https://github.com/Soverzion-HQ/vendor_lineage-priv vendor/lineage-priv
+git clone https://github.com/Soverzion-Garnet/hardware_qcom-caf_sm8450_display -b 14 hardware/qcom-caf/sm8450/display
+git clone https://github.com/Soverzion-Garnet/vendor_lineage-priv vendor/lineage-priv
 
-echo 'Cloning completed. It's time to lunch. Miaw'
+echo 'Cloning completed. Time to lunch. Miaw'
